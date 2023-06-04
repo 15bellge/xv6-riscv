67eb678baffcf264c468bc1ca3b44414633accd9
15bellge <94174826+15bellge@users.noreply.github.com>
2023-06-04 09:47:50 +0300

    task 1 finish

--- user/ustack.c
+++ user/ustack.c
@@ -4,7 +4,7 @@ struct Header
 {
     uint len;
     uint dealloc_page;
-    struct header *prev;
+    struct Header *prev;
 };
 
 static struct Header *header_ptr = 0;
@@ -14,7 +14,7 @@ static char *page_ptr;
 
 void *ustack_malloc(uint len)
 {
-    struct Header *hp;
+    struct Header *hp = 0;
     char *page;
 
     if (len > MAX_ALLOC)
@@ -38,7 +38,7 @@ void *ustack_malloc(uint len)
         return (void *)stack_ptr;
     }
 
-    if (page_ptr - page_ptr < len)
+    if (page_ptr - stack_ptr < len)
     {
         page = sbrk(PGSIZE);
         if (page == (char *)-1)
@@ -48,6 +48,7 @@ void *ustack_malloc(uint len)
         page_ptr = page + PGSIZE;
         hp->len = len;
         hp->prev = header_ptr;
+        hp->dealloc_page = 1;
         stack_ptr = (char *)hp + len;
         header_ptr = hp;
         return (void *)stack_ptr;
@@ -69,12 +70,14 @@ int ustack_free(void)
     char *page;
     int header_size = header_ptr->len;
     header_ptr = header_ptr->prev;
-    if(header_ptr->dealloc_page == 1){
+    if (header_ptr->dealloc_page == 1)
+    {
         page = sbrk(-PGSIZE);
-        if(p==(char*)-1){
+        if (page == (char *)-1)
+        {
             return -1;
         }
-        page_ptr = page_ptr - PGSIZE;
+        page_ptr -= PGSIZE;
     }
     return header_size;
 }
